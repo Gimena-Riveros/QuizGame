d73b330a2fd3d051d2d4e979660e5cbe7b8b17ba
GimenaRiveros <gimena1001riveros@gmail.com>
2022-12-17 17:24:40 -0300

    QuizGame, agregamos un boton de ayuda con una pista texto e imagen gracias a la funcion helper en el index.js . MGR

--- js/dbQuestions.js
+++ js/dbQuestions.js
@@ -2,9 +2,10 @@ let dbQuestions = [
     {
         //"cathegory":"Art and literature",
         question:"How paint the Mona Lisa?",
-        help: "Tabién pintó la última cena",
+        help: "That person also painted \"the last supper\"",
+        imgHelp: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs3ydoI4NVb0iVXSzR-5nzK5zOoeMWjXioJw&usqp=CAU",
         images:"https://historia.nationalgeographic.com.es/medio/2018/06/11/01-mona-lisa-marco-gioconda-louvre_2d7f91a1.jpg",
         answer:"Leonardo daVinci",
-        badanswers : ["Miguel Angel","Beethoven","Monet","Picasso"],        
+        badanswers : ["Miguel Angel","Anguissola","Picasso"],        
     },    
 ];
--- js/index.js
+++ js/index.js
@@ -1,6 +1,29 @@
 loadQuestions(0)
 
 function loadQuestions(index) {
-    let objectQuestion = dbQuestions[index]
+    // al sacar let la variable persiste mas alla de esta funcion, ahora es global
+    objectQuestion = dbQuestions[index]
+    let options = [...objectQuestion.badanswers]
+    // badanswers + answer
+    options.push(objectQuestion.answer)
+    // double random options
+    for(let i = 0 ; i<4 ; i++) {
+        options.sort(() => Math.random()-0.5);
+    }
+
     document.getElementById("question").innerHTML = objectQuestion.question
+    document.getElementById("option-01").innerHTML = options[0]
+    document.getElementById("option-02").innerHTML = options[1]
+    document.getElementById("option-03").innerHTML = options[2]
+    document.getElementById("option-04").innerHTML = options[3]
+    
+}
+// funcionalidad cortesia de sweetalert
+function helper() {
+    Swal.fire({
+        title: "Help!",
+        text: objectQuestion.help,
+        imageUrl: objectQuestion.imgHelp,
+        imageHeight: 250,
+      });
 }
